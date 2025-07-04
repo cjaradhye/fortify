95ec78e0474696762df1a7448fed8576fd737f56
aradhye <aradhye.swarup@gmail.com>
2025-07-04 11:42:47 +0530

    deployed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
-// src/App.js
-import React, { useState } from "react";
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import SolidityIDE from "./components/SolidityIDE";
 import Navbar from "./components/Navbar";
 import Landing from "./components/landing/Landing";
-// import Sample from "./components/Sample";
-import ToggleButton from "./components/MainThingy";
-import { Routes, Route } from 'react-router-dom'
+import MainThingy from "./components/MainThingy";
 import ContractFetcher from "./components/ContractFetcher";
 import { ToastContainer } from "react-toastify";
-import MainThingy from "./components/MainThingy";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={
-        <div className="plswork">
-          <MainThingy />
-        </div>
-        } />
-      <Route path="/" element={
-        <Landing />} />
+      <Route path="/home" element={<div className="plswork"><MainThingy /></div>} />
+      <Route path="/" element={<Landing />} />
       <Route path="/contract" element={
         <>
-        <ToastContainer />
-        <ContractFetcher />
-        </>} />
+          <ToastContainer />
+          <ContractFetcher />
+        </>
+      } />
     </Routes>
-  )
+  );
 }
 
 export default App;
-
